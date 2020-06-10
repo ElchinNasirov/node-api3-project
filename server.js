c9c077016d373597e736dc73828bb9cf9e8df4a6
ElchinNasirov <nasirov.elchin@gmail.com>
2020-06-09 17:23:07 -0700

    Initial commit, add some code

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const userRouter = require("./users/userRouter");
+const postRouter = require("./posts/postRouter");
 
 const server = express();
+
+server.use(express.json());
+
+server.use("/api/users", logger, userRouter);
+server.use("/api/posts", postRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
 
-function logger(req, res, next) {}
+//custom middleware
+function logger(req, res, next) {
+  const time = new Date.toISOString();
+
+  console.log(`${time} ${req.ip} ${req.method} ${req.url}`);
+  next()
+}
 
 module.exports = server;
